6171cc0bdabbac43ad5064af4dbe4bc2bc8c53d1
asangom1010 <asangom1010@g.educaand.es>
2025-02-22 19:06:34 +0100

    terminado
    terminado

--- script.js
+++ script.js
@@ -40,3 +40,13 @@ document.addEventListener("DOMContentLoaded", function () {
         });
       });
       
+
+      window.addEventListener("scroll", function () {
+        var navbar = document.querySelector(".navbar");
+        if (window.scrollY > 50) {
+          navbar.classList.add("scrolled");
+        } else {
+          navbar.classList.remove("scrolled");
+        }
+      });
+      
